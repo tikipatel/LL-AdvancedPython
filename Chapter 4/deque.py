282c04075b5c1462f536aa198ff30626371ac8b3
Pratikbhai Patel <pratikpatel@me.com>
2020-04-18 23:22:54 -0700

    Deques

--- Chapter 4/deque.py
+++ Chapter 4/deque.py
@@ -15,10 +15,28 @@ import string
 def main():
     print('__main__')
     # initialize a deque with lowercase characters
+    d = collections.deque(string.ascii_lowercase)
+    
     # deques support the len() function
+    print('Item count:', len(d))
+    
     # deques can be iterated over
+    print('Uppercased the lowercased deque:')
+    for elem in d:
+        print(elem.upper(), end=', ')
+    print('---')
     # manipulate items from either end
+    d.pop()
+    d.popleft()
+    
+    d.append(2)
+    d.appendleft(1)
+    print(d)
+
     # rotate the deque
+    print(d)
+    d.rotate(10)
+    print(d)
 
 
 if __name__ == '__main__':
